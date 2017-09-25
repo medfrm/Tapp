0c98921ab2015292b203436029733ec116dd0c80
medfrm <32132651+medfrm@users.noreply.github.com>
2017-09-25 16:52:04 -0700

    model modif

--- models/node.js
+++ models/node.js
@@ -2,34 +2,36 @@ var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var url = 'mongodb://localhost:27017/Tapp';
 
-var node = function node()
+var node = function node(data)
 {
-  this.index = () => {};
-  this.owner = {};
-  this.list = [{names : "", numbers : ""}];
-  this.indices : (this.list) => {};
-};
+  this.index = () => {
+    MongoClient.connect(url, function(err, db)
+    {
+      assert.equal(null, err);
+      var collection = db.collection('nodes');
+      //...
+      db.close();
+    });
+  };
 
-MongoClient.connect(url, function(err, db)
-{
-  assert.equal(null, err);
-  console.log("Connected correctly to server");
-  db.close();
-});
+  this.owner = {name : data.owner.name, phone : data.owner.phone};
+  this.list = [{names : "", numbers : ""}];
+  this.indices : (this.list) => {
+    //...
+  };
+};
 
 var insertNode = function(db, callback)
 {
-  var collection = db.collection('documents');
-
+  var collection = db.collection('nodes');
   collection.insertOne(node, function(err, result) {
     assert.equal(err, null);
-    //assert.equal(3, result.result.n);
-    //assert.equal(3, result.ops.length);
+    //...
     console.log("Inserted object into the document collection");
     callback(result);
   });
 }
 
-var 
+
 
 module.exports = dbs;
